a0613f1103a255ace5b94a3289aaa8a5efeb28a4
Shivam Singh <147870844+07shi@users.noreply.github.com>
2026-05-23 00:38:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (45.24%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,38 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int mid;
-        int low = 0;
-        int high = nums.length - 1;
-        while(low<=high){
-            mid = (low+high)/2;
+        int st = 0;
+        int end = nums.length - 1;
+        while(st<=end){
+            int mid = st + (end - st)/2;
             if(nums[mid]==target){
                 return mid;
-
             }
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target <= nums[mid]){
-                    high = mid - 1;
-
+            if(nums[st]<=nums[mid]){
+                if(target >= nums[st] && target < nums[mid]){
+                    end = mid - 1;
                 }
                 else{
-                    low = mid + 1;
+                    st = mid + 1;
                 }
-
-
-                
             }
             else{
-                if(nums[mid] <= target && target <= nums[high]){
-                    low  = mid + 1;
-
+                if(target > nums[mid] && target <= nums[end]){
+                    st = mid + 1;
                 }
                 else{
-                    high = mid - 1;
+                    end = mid - 1;
                 }
             }
-            
         }
         return -1;
-        
     }
 }
